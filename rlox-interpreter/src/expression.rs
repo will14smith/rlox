f48f52872322fb06319cb8bf909d44cc834250c6
Will Smith <will@toxon.co.uk>
2020-01-05 18:05:46 +0000

    Implement evaluator for grouping & unary

--- rlox-interpreter/src/expression.rs
+++ rlox-interpreter/src/expression.rs
@@ -1,3 +1,4 @@
+use rlox_scanner::Token;
 use rlox_parser::Expr;
 use crate::Value;
 
@@ -8,15 +9,35 @@ pub fn evaluate(expr: &Expr) -> Value {
         Expr::Number(value) => Value::Number(*value),
         Expr::String(value) => Value::String(value.clone()),
 
+        Expr::Grouping(expr) => evaluate(expr),
+
+        Expr::Unary(op, expr) => {
+            let value = evaluate(expr);
+
+            match &op.token {
+                Token::Minus => Value::Number(-value.as_number()),
+                Token::Bang => Value::Boolean(!value.is_truthy()),
+
+                _ => panic!("Invalid unary operation {:?}", op.token)
+            }
+        },
+
         Expr::Binary(_, _, _) => unimplemented!(),
-        Expr::Unary(_, _) => unimplemented!(),
-        Expr::Grouping(_) => unimplemented!(),
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use rlox_scanner::{ SourceToken };
     use super::*;
+
+    fn tok_to_src(t: Token) -> SourceToken {
+        SourceToken {
+            token: t,
+            lexeme: String::new(),
+            line: 0
+        }
+    }
 
     #[test]
     fn test_literal() {
@@ -24,5 +45,23 @@ mod tests {
         assert_eq!(evaluate(&Expr::Boolean(true)), Value::Boolean(true));
         assert_eq!(evaluate(&Expr::Number(123f64)), Value::Number(123f64));
         assert_eq!(evaluate(&Expr::String("abc".into())), Value::String("abc".into()));
+    }
+
+    #[test]
+    fn test_grouping() {
+        assert_eq!(evaluate(&Expr::Grouping(Box::new(Expr::Boolean(true)))), Value::Boolean(true));
+    }
+
+    #[test]
+    fn test_unary() {
+        assert_eq!(evaluate(&Expr::Unary(tok_to_src(Token::Minus), Box::new(Expr::Number(123f64)))), Value::Number(-123f64));
+        assert_eq!(evaluate(&Expr::Unary(tok_to_src(Token::Bang), Box::new(Expr::Boolean(true)))), Value::Boolean(false));
+    }
+
+    #[test]
+    fn test_unary_runtime_error() {
+        // TODO
+        // evaluate(&Expr::Unary(tok_to_src(Token::Minus), Box::new(Expr::Boolean(true))))
+        // evaluate(&Expr::Unary(tok_to_src(Token::Bang), Box::new(Expr::Number(123f64))))
     }
 }
--- rlox-interpreter/src/value.rs
+++ rlox-interpreter/src/value.rs
@@ -5,3 +5,22 @@ pub enum Value {
     Number(f64),
     String(String),
 }
+
+impl Value {
+    pub fn as_number(&self) -> f64 {
+        match self {
+            Value::Number(value) => *value,
+
+            Value::Boolean(_) | Value::String(_) | Value::Nil => unimplemented!("handle runtime error"),
+        }
+    }
+
+    pub fn is_truthy(&self) -> bool {
+        match self {
+            Value::Nil => false,
+            Value::Boolean(value) => *value,
+            Value::Number(_) => true,
+            Value::String(_) => true,
+        }
+    }
+}
